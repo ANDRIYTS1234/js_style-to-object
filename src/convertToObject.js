145c747dc7719fcd849a1cf5ee294d3f72103cd6
Andrii Tsopych <andriythoupich01@gmail.com>
2025-12-24 18:14:47 +0200

    added task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,21 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const result = {};
+
+  const properties = sourceString.split(';');
+
+  properties.forEach((property) => {
+    const propertyCopy = property.trim();
+
+    const partsOfProperty = propertyCopy.split(':');
+
+    if (partsOfProperty.length === 2) {
+      result[partsOfProperty[0].trim()] = partsOfProperty[1].trim();
+    }
+  });
+
+  return result;
 }
 
 module.exports = convertToObject;
